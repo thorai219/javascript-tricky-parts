90c423525d869377314bfc6c4c8a36323e538d2d
Terry Lee <slsk0219@gmail.com>
2020-12-15 11:17:17 -0800

    couple problems done

--- create-account.js
+++ create-account.js
@@ -1,5 +1,29 @@
-function createAccount(pin, amount) {
-
+function createAccount(pin, amount = 0) {
+  return {
+    checkBalance(pinInput) {
+      if (pin !== pinInput) return "Invalid PIN";
+      return `$${amount}`;
+    },
+    deposit(pinInput, depositAmount) {
+      if (pin !== pinInput) return "Invalid PIN";
+      amount += depositAmount;
+      return `Successfully deposited ${depositAmount}, Current balance: $${amount}`;
+    },
+    widthdraw(pinInput, withdrawAmount) {
+      if (pin !== pinInput) return "Invalid PIN";
+      if (withdrawAmount > amount) {
+        return "Withdrawl amount exceeds account balance. Transaction cancelled";
+      } else {
+        amount -= withdrawAmount;
+        return `Succressfully withdrew ${withdrawAmount}, Current balance: $${amount}`;
+      }
+    },
+    changePin(pinInput, newPin) {
+      if (pin !== pinInput) return "Invalid PIN";
+      pin = newPin;
+      return "PIN changed successfully!";
+    },
+  };
 }
 
 module.exports = { createAccount };
--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,19 @@
 function guessingGame() {
+  const answer = Math.floor(Math.random() * 100);
+  let guessed = false;
+  let numOfGuesses = 0;
 
+  return function guess(num) {
+    if (guessed) return "You've won already!";
+    numOfGuesses++;
+    if (num === answer) {
+      guessed = true;
+      let guesses = numOfGuesses === 1 ? "guess" : "guesses";
+      return `You win! You guessed ${num} in ${numOfGuesses} ${guesses}`;
+    }
+    if (num < answer) return `${num} is too low!`;
+    if (num > answer) return `${num} is too high!`;
+  };
 }
 
 module.exports = { guessingGame };
